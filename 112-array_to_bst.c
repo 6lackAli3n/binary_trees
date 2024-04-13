a666e6c4cd7a0b109e083f9d1a6f8db60ecb0ced
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-04-13 01:48:14 +0100

    binary

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * array_to_bst - Builds a Binary Search Tree from an array
  * @array: A pointer to the first element of the array to be converted
@@ -12,16 +13,32 @@ bst_t *array_to_bst(int *array, size_t size)
 	bst_t *root = NULL;
 	size_t i;
 
-	if (!array || size == 0)
+	if (array == NULL || size == 0)
 		return (NULL);
 
 	/* Insert each element from the array into the BST */
 	for (i = 0; i < size; i++)
 	{
-		if (!bst_insert(&root, array[i]))
-			return (NULL); /* Ignore duplicate values */
+		if (bst_insert(&root, array[i]) == NULL)
+		{ 
+			bst_free(root);
+			return (NULL);
+		}
 	}
 
 	return (root);
 }
 
+/**
+ * bst_free - Frees a binary search tree
+ * @tree: Pointer to the root node of the tree to free
+ */
+ void bst_free(bst_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	bst_free(tree->left);
+	bst_free(tree->right);
+	free(tree);
+}
